0f4b15cab5661d7ec8b06833f191735c01c1b441
Suresh Thomas <st185229@ncr.com>
2021-01-19 02:01:29 +0000

    Convert the Pricing Service - Other than the test

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,23 +1,39 @@
 package com.udacity.pricing.domain.price;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
-
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
 public class Price {
-
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
     private String currency;
     private BigDecimal price;
-    private Long vehicleId;
-
     public Price() {
     }
 
-    public Price(String currency, BigDecimal price, Long vehicleId) {
+    public Price(Long id, String currency, BigDecimal price) {
+        this.id = id;
         this.currency = currency;
         this.price = price;
-        this.vehicleId = vehicleId;
+    }
+
+    public Price(String currency, BigDecimal price) {
+        this.currency = currency;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getCurrency() {
@@ -34,13 +50,5 @@ public class Price {
 
     public void setPrice(BigDecimal price) {
         this.price = price;
-    }
-
-    public Long getVehicleId() {
-        return vehicleId;
-    }
-
-    public void setVehicleId(Long vehicleId) {
-        this.vehicleId = vehicleId;
     }
 }
---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO PRICE(id,currency,price) VALUES(1,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(2,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(3,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(4,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(5,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(6,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(7,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(8,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(9,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(10,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(11,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(12,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(13,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(14,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(15,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(16,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(17,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(18,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(19,'ISD', 4579.89);
+INSERT INTO PRICE(id,currency,price) VALUES(20,'ISD', 4579.89);
+
+
+
